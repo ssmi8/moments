ad4a516f94d85ce23f6ce9e9e092a2114d719371
ssmi8 <smithsteve8@hotmail.com>
2022-12-28 16:50:09 +0000

    Redirecting users

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 
-import Form  from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -18,8 +18,10 @@ import btnStyles from "../../styles/Button.module.css";
 
 import { useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function PostCreateForm() {
+  useRedirect("loggedOut");
   const [errors, setErrors] = useState({});
 
   const [postData, setPostData] = useState({
